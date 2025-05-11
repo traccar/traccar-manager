5d43b14d7a29fd06f02fe4ccbfc9064324b5885e
Anton Tananaev <anton.tananaev@gmail.com>
2025-05-11 07:11:05 -0700

    Use preferences for url

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -9,21 +10,39 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
-  late final WebViewController _controller;
+  late final Future<WebViewController> _webViewControllerFuture;
 
   @override
   void initState() {
     super.initState();
-    _controller = WebViewController()
+    _webViewControllerFuture = _initWebView();
+  }
+
+  Future<WebViewController> _initWebView() async {
+    final preferences = await SharedPreferences.getInstance();
+    final url = preferences.getString('url') ?? 'https://demo.traccar.org'; //'http://localhost:3000';
+
+    final controller = WebViewController()
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..loadRequest(Uri.parse('http://localhost:3000'));
+      ..loadRequest(Uri.parse(url));
+
+    return controller;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: WebViewWidget(controller: _controller),
+        child: FutureBuilder<WebViewController>(
+          future: _webViewControllerFuture,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done && snapshot.hasData) {
+              return WebViewWidget(controller: snapshot.data!);
+            } else {
+              return const Center(child: CircularProgressIndicator());
+            }
+          },
+        ),
       ),
     );
   }
